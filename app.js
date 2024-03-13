11a4639149fa24f1fda1e0e4a9a1951c961f54eb
egecan12 <egecan18k@gmail.com>
2024-03-13 13:18:01 -0400

    Add global error handling middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 //MIDDLEWARES
 
@@ -28,4 +30,6 @@ app.use('/api/v1/tours', tourRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+app.use(globalErrorHandler);
+
 module.exports = app;
