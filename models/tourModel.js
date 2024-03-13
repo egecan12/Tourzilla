96fca2ed05997e528b24ef3f2398751a021f9c2c
egecan12 <egecan18k@gmail.com>
2024-03-13 10:49:15 -0400

    Add query middleware to exclude secret tours and active users

--- models/tourModel.js
+++ models/tourModel.js
@@ -131,6 +131,24 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// QUERY MIDDLEWARE
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+// tourSchema.pre(/^find/, function (next) {
+//   this.populate({
+//     path: 'guides',
+//     select: '-__v -passwordChangedAt',
+//   });
+
+//   next();
+// });
+
 // tourSchema.pre('save', async function(next) {
 //   const guidesPromises = this.guides.map(async id => await User.findById(id));
 //   this.guides = await Promise.all(guidesPromises);
